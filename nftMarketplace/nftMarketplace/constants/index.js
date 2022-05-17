cca593a1b714a880efa91613dfeeeac55cd75b7c
droigon <ude.chekwube@gmail.com>
2022-05-17 01:37:28 +0100

    sec

---
+++ nftMarketplace/nftMarketplace/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x546d54aD758352064ddBaE01EF026C92e6ab0aD7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-marketplace-five-theta.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
